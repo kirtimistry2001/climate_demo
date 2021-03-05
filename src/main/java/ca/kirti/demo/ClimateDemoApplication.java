27d58a6c05c7924d7d084b1acf84feb0ef96b28d
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-03-04 20:22:08 -0500

    Created composite primary key and save data from CSV file to H2 in memory database on application startup.

--- src/main/java/ca/kirti/demo/ClimateDemoApplication.java
+++ src/main/java/ca/kirti/demo/ClimateDemoApplication.java
@@ -1,13 +1,25 @@
 package ca.kirti.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ClimateDemoApplication {
+import ca.kirti.demo.service.ClimateSummaryService;
 
+@SpringBootApplication
+public class ClimateDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClimateSummaryService csService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClimateDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		csService.saveClimateSummaryData();
 	}
 
 }
--- src/main/java/ca/kirti/demo/model/ClimateSummary.java
+++ src/main/java/ca/kirti/demo/model/ClimateSummary.java
@@ -2,81 +2,112 @@ package ca.kirti.demo.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+/**
+ * Model class for ClientSummary data.
+ * Mapping Composite Key using @IdClass Annotation
+ * @author Kirti
+ * 
+ *
+ */
 @Entity
+@Table(name="climate_summary")
+@IdClass(ClimateSummaryIdentity.class)
 public class ClimateSummary {
+
+	// Mapping Composite Key
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
-	
-	private String station_Name;
+	private String stationName;
+	@Id
 	private String province;	
-	private Date date_;	
+	@Id
+	@Temporal(TemporalType.DATE)
+	private Date climateDate;
+
+	@Column(name ="meanTemp",nullable = true)
 	private float mean_Temp;
+
+
+	@Column(name="hMMTemp", nullable = true)
 	private float highest_Monthly_Maxi_Temp	;
+
+	@Column(name="lMMTemp",nullable = true)
 	private float lowest_Monthly_Min_Temp;
-	
+
 	public ClimateSummary() {}
-	
-	
-	public ClimateSummary(String station_Name, String province, Date date_, float mean_Temp,
+
+	public ClimateSummary(String stationName, String province, Date climateDate, float mean_Temp,
 			float highest_Monthly_Maxi_Temp, float lowest_Monthly_Min_Temp) {
 		super();
-		this.station_Name = station_Name;
+		this.stationName = stationName;
 		this.province = province;
-		this.date_ = date_;
+		this.climateDate = climateDate;
 		this.mean_Temp = mean_Temp;
 		this.highest_Monthly_Maxi_Temp = highest_Monthly_Maxi_Temp;
 		this.lowest_Monthly_Min_Temp = lowest_Monthly_Min_Temp;
 	}
 
 
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getStation_Name() {
-		return station_Name;
-	}
-	public void setStation_Name(String station_Name) {
-		this.station_Name = station_Name;
-	}
-	public String getProvince() {
-		return province;
-	}
-	public void setProvince(String province) {
-		this.province = province;
-	}
-	public Date getDate_() {
-		return date_;
-	}
-	public void setDate_(Date date_) {
-		date_ = date_;
-	}
 	public float getMean_Temp() {
 		return mean_Temp;
 	}
-	public void setMean_Temp(float mean_Temp) {
-		mean_Temp = mean_Temp;
+
+	public String getStationName() {
+		return stationName;
 	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public Date getClimateDate() {
+		return climateDate;
+	}
+
+	public void setClimateDate(Date climateDate) {
+		this.climateDate = climateDate;
+	}
+
+	public void setMean_Temp(float mean_Temp) {
+		this.mean_Temp = mean_Temp;
+	}
+
 	public float getHighest_Monthly_Maxi_Temp() {
 		return highest_Monthly_Maxi_Temp;
 	}
+
 	public void setHighest_Monthly_Maxi_Temp(float highest_Monthly_Maxi_Temp) {
-		highest_Monthly_Maxi_Temp = highest_Monthly_Maxi_Temp;
+		this.highest_Monthly_Maxi_Temp = highest_Monthly_Maxi_Temp;
 	}
+
 	public float getLowest_Monthly_Min_Temp() {
 		return lowest_Monthly_Min_Temp;
 	}
+
 	public void setLowest_Monthly_Min_Temp(float lowest_Monthly_Min_Temp) {
-		lowest_Monthly_Min_Temp = lowest_Monthly_Min_Temp;
+		this.lowest_Monthly_Min_Temp = lowest_Monthly_Min_Temp;
 	}
 
+	@Override
+	public String toString() {
+		return "ClimateSummary [stationName=" + stationName + ", province=" + province + ", climateDate=" + climateDate
+				+ ", mean_Temp=" + mean_Temp + ", highest_Monthly_Maxi_Temp=" + highest_Monthly_Maxi_Temp
+				+ ", lowest_Monthly_Min_Temp=" + lowest_Monthly_Min_Temp + "]";
+	}
 
 }
