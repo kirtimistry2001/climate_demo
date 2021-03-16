3591c53f9b5ed262b5bf7269d8e9d12c903addc7
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-03-16 15:20:15 -0400

    Added documentation for finder methods in repository.

--- src/main/java/ca/kirti/demo/repositories/ClimateSummaryRepository.java
+++ src/main/java/ca/kirti/demo/repositories/ClimateSummaryRepository.java
@@ -15,10 +15,25 @@ import ca.kirti.demo.model.ClimateSummaryIdentity;
 @Repository
 public interface ClimateSummaryRepository extends JpaRepository<ClimateSummary, ClimateSummaryIdentity> {
 
+	/**
+	 * Returns data for given date range.
+	 * @param dateFrom
+	 * @param dateTo
+	 * @return
+	 */
 	@Query("select c from ClimateSummary c " +
 	         "where c.climateDate between ?1 and ?2")
 	List<ClimateSummary> findByDatesBetween(Date dateFrom, Date dateTo);
 	
+	/**
+	 * Returns a {@link Page}for given date range and 
+	 * 		meeting the paging restriction provided in the {@code Pageable} object
+	 * @param dateFrom
+	 * @param dateTo
+	 * @param pageable
+	 * 	the paging restriction provided in the {@code Pageable} object
+	 * @return
+	 */
 	@Query("select c from ClimateSummary c " +
 	         "where c.climateDate between ?1 and ?2")
 	Page<ClimateSummary> findByDatesBetween(Date dateFrom, Date dateTo, Pageable pageable);
